ec5c5fe7ef59ecd370659db545861e7359ae1a8d
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2020-01-07 14:53:34 -0500

    Fix seeding

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app, port } from "./app";
 import mongoose from "mongoose";
+import { seedUsers } from "./database/seeds/seedSetup";
 
 const dbUsername = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
@@ -11,9 +12,22 @@ mongoose.connect(`mongodb://localhost:27017/wisp`, {
 });
 mongoose.set("useCreateIndex", true);
 
+async function seed() {
+    try {
+        await seedUsers();
+        // tslint:disable-next-line:no-console
+        console.log("Seeding successful");
+    } catch (error) {
+        // tslint:disable-next-line:no-console
+        console.log(`Failure with seeding: ${error}`);
+        throw error;
+    }
+}
+
 const server = app.listen(port, () => {
     // tslint:disable-next-line:no-console
     console.log(`Server started at http://localhost:${port}`);
+    seed()
 });
 
 export { server };
