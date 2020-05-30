717838c064fa3cb4a4c960ba49f96302c0aa4537
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2020-05-30 13:02:01 -0400

    Minor changes

--- src/validators/userCustom.ts
+++ src/validators/userCustom.ts
@@ -1,8 +1,8 @@
 
-export function validUsername(username: string): boolean {
+export const validUsername = (username: string): boolean => {
     return new RegExp(/^[a-zA-Z0-9._-]*$/).test(username);
 }
 
-export function validPassword(password: string): boolean {
-    return password.length > 5 && password.indexOf(" ") === -1;
+export const validPassword = (password: string): boolean => {
+    return (password.length > 5) && (password.indexOf(" ") === -1);
 }
